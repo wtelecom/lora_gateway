3ba38df6b340139319747c20e4b430141b2f5f3f
Nestor Ayuso <nestorayuso@gmail.com>
2016-08-19 11:01:16 +0200

    change clock source
    Baseband is clocked by radio A instead of radio B

--- util_tx_test/src/util_tx_test.c
+++ util_tx_test/src/util_tx_test.c
@@ -118,7 +118,7 @@ int main(int argc, char **argv)
 	
 	/* RF configuration (TX fail if RF chain is not enabled) */
 	enum lgw_radio_type_e radio_type = LGW_RADIO_TYPE_NONE;
-	uint8_t clocksource = 0; /* Radio A is source by default */
+	uint8_t clocksource = 0; /* Radio A is source in MTAC-LORA */
 	struct lgw_conf_board_s boardconf;
 	struct lgw_conf_rxrf_s rfconf;
 	
@@ -255,7 +255,7 @@ int main(int argc, char **argv)
 				invert = true;
 				break;
 
-			case 'k': /* <int> Concentrator clock source (Radio A or Radio B) */
+			case 'k': /* <int> Concentrator clock source (Radio A(default in MTAC-LORA) or Radio B) */
 				sscanf(optarg, "%i", &xi);
 				clocksource = (uint8_t)xi;
 				break;
